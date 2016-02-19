d28f32cc5d5406ab5e0bfee53b7d0a4e93f88e8d
Ayaz Nawaz <ayaz6699@gmail.com>
2016-02-19 15:52:49 +0400

    display of user and time on index pag-article page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-validates :title, presence: true, length:{minimum: 3, maximum: 10}
+belongs_to :user
+validates :title, presence: true, length:{minimum: 10, maximum: 20}
 validates :description, presence: true, length:{minimum: 10, maximum: 200}
-
+validates :user_id, presence: true
 end
