4d584ccd7c3e58794ba96a5a61e65d71fe31b15b
Ayaz Nawaz <ayaz6699@gmail.com>
2016-02-24 10:42:28 +0400

    Add pagination to views of specific user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 def index
-	@users = User.all
+	@users = User.paginate(page: params[:page], per_page: 2)
 end
 
 def new
@@ -22,9 +22,6 @@ def create
 		end
 end
 
-
-	
-
 def update
 	@user = User.find(params[:id])
 		if @user.update(user_params)
@@ -37,6 +34,7 @@ end
 
 def show
 	@user = User.find(params[:id])
+	@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
 end
 
 private
