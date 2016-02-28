630047118e9d2863f92d07ff5612cf2bb85cd8c9
Ayaz Nawaz <ayaz6699@gmail.com>
2016-02-28 11:59:08 +0400

    Add admin user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219133212) do
+ActiveRecord::Schema.define(version: 20160227220412) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160219133212) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
