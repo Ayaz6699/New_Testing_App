f79c30e687843e0ec9a5197722870244f1d7433f
Ayaz Nawaz <ayaz6699@gmail.com>
2016-03-09 21:47:58 +0400

    Add artcile and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,14 @@
 class Article < ActiveRecord::Base
 
+	has_many :article_categories
+	has_many :categories, through: :article_categories
+
 	belongs_to :user
-validates :title, presence: true, length:{minimum: 3, maximum: 10}
+	validates :title, presence: true, length:{minimum: 3, maximum: 10}
 
-belongs_to :user
-validates :title, presence: true, length:{minimum: 10, maximum: 20}
+	belongs_to :user
+	validates :title, presence: true, length:{minimum: 10, maximum: 20}
 
-validates :description, presence: true, length:{minimum: 10, maximum: 200}
-validates :user_id, presence: true
+	validates :description, presence: true, length:{minimum: 10, maximum: 200}
+	validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160308164725) do
 
-ActiveRecord::Schema.define(version: 20160301053806) do
-
+  create_table "article_categories", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,14 +26,12 @@ ActiveRecord::Schema.define(version: 20160301053806) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-
   end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-
   end
 
   create_table "users", force: :cascade do |t|
